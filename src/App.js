d9c607ecfbe4757406c9fec21dc8c4f5d6577990
Al3ncar <alencar29htcss@gmail.com>
2023-05-19 23:20:51 -0300

    ToDo_List

--- src/App.js
+++ src/App.js
@@ -1,84 +1,79 @@
-import React from "react";
-import "./App.css";
-import styled from "styled-components";
+import React from "react"; import "./App.css";
 import trash from "./img/lixo.png";
 
-const Main = styled.main`
-  width: 80vw;
-  background-color: whitesmoke;
-`;
-const Rod = styled.div`
-  width: 100%;
-  background-color: #553d32;
-  height: 5vh;
-`;
-const Boxtitle = styled.div`
-  width: 90%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: greem;
-  margin-left: 4.5%;
-  border-bottom: solid gray 0.2vw;
-`;
-const Data = styled.div`
-  width: 15%;
-  height: 50%;
-  margin: 2% 3% 5% 5%;
-  display: flex;
-  justify-content: center;
-  flex-direction: row;
-  outline: 0;
-`;
-const Title = styled.h1`
-  width: 100%;
-  padding: 2% 4% 0 4%;
-  font-size: 3vw;
-`;
-const Newtesk = styled.div`
-  width:100%;
-  display:flex;
-  justify-content:space-around;
-  align-items:center;`;
-const Del = styled.button`
-  width: 10%;
-  padding: 0.5%;
-  margin-left: 5.5%;
-`;
-const Btn = styled.button`
-  width: 10%;
-  padding: 0.5%;
-`;
-const Task = styled.input`
-  width: 60%;
-  padding: 0.5% 1%;
-  margin: 2% 0% 2% 0%;
-  outline: 0;
-`;
-const TaskList = styled.li`
-  width: 75%;
-  padding-left: 1%;
-  margin: 2% 7%;
-  border-bottom: solid gray 0.2vw;
-  list-style: square;
-`;
-const Listbox = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  background-color: whitesmoke;
-`;
-const Trash = styled.img`
-  width: 2%;
-  height: 3%;
-  padding-top: 1.5%;
-`;
-const List = styled.div`
-  width: 100%;
-  display: flex;
-`;
+import styled from "styled-components";
+  const Main = styled.main`
+    width: 80vw;
+    background-color: whitesmoke;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  `;
+  const Rod = styled.div`
+    width: 100%;
+    background-color: #553d32;
+    height: 5vh;
+  `;
+  const Boxtitle = styled.div`
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: greem;
+
+    border-bottom: solid gray 0.2vw;
+  `;
+  const Data = styled.div`
+    width: 15%;
+    height: 50%;
+    margin: 2% 3% 5% 5%;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    outline: 0;
+  `;
+  const Title = styled.h1`
+    width: 100%;
+    padding: 2% 4% 0 4%;
+    font-size: 3vw;
+  `;
+  const Newtesk = styled.div`
+    width:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;`;
+  const Del = styled.button` width: 10%; padding: 0.5%;`;
+  const Btn = styled.button` width: 10%; padding: 0.5%;`;
+  const Task = styled.input`
+    width: 60%;
+    padding: 0.5% 1%;
+    margin: 2% 0% 2% 0%;
+    outline: 0;
+  `;
+  const TaskList = styled.li`
+    width: 75%;
+    padding-left: 1%;
+    margin: 2% 7%;
+    border-bottom: solid gray 0.2vw;
+    list-style: square;
+  `;
+  const Listbox = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background-color: whitesmoke;
+  `;
+  const Trash = styled.img`
+    width: 2%;
+    height: 3%;
+    padding-top: 1.5%;
+  `;
+  const List = styled.div` width: 100%; display: flex;`;
+
 export default class ToDo extends React.Component {
+  
   state = { task: "", list: [] };
   handleChange = (ev) => { this.setState({ task: ev.target.value }); };
   add = () => {
@@ -103,7 +98,7 @@ export default class ToDo extends React.Component {
         <Boxtitle>
           <Title>Lista de Tarefas</Title>
           <Data>
-            <input type="date" />
+            <input type="date"/>
           </Data>
         </Boxtitle>
         <Listbox>
@@ -116,7 +111,6 @@ export default class ToDo extends React.Component {
             />
             <Btn onClick={this.add}>Add Tarefa</Btn>
           </Newtesk>
-
           {list.map((item) => (
             <ul>
               <List>
